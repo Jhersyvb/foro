73e6d8450da11a83d34fa3cac5959e9283b1ec0a
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2017-12-08 13:00:08 -0500

    List records with TDD in Laravel 5.3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,15 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post, $slug)
     {
-        // abort_unless($post->slug == $slug, 404);
-       // abort_if($post->slug != $slug, 404);
-
         if ($post->slug != $slug) {
             return redirect($post->url, 301);
         }

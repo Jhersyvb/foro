38a366078d7d54e94485efec773eb4b20c8f0162
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-03-01 10:58:14 -0500

    Autenticación basasa en tokens con Laravel 5.3 y TDD

--- tests/features/AuthenticationTest.php
+++ tests/features/AuthenticationTest.php
@@ -1,87 +1,30 @@
 <?php
 
 use App\Token;
-use App\Mail\TokenMail;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class AuthenticationTest extends FeatureTestCase
+class AuthenticationTest extends TestCase
 {
-    function test_a_guest_user_can_request_a_token()
+    function test_a_user_can_login_with_a_token_url()
     {
         // Having
-        Mail::fake();
+        $user = $this->defaultUser();
 
-        $user = $this->defaultUser(['email' => 'hola@jhersy.com']);
-
-        // When
-        $this->visitRoute('login')
-            ->type('hola@jhersy.com', 'email')
-            ->press('Solicitar token');
-
-        // Then: a new token is created in the database
-        $token = Token::where('user_id', $user->id)->first();
-
-        $this->assertNotNull($token, 'A token was not created');
-
-        // And sent to the user
-        Mail::assertSentTo($user, TokenMail::class, function ($mail) use ($token) {
-            return $mail->token->id === $token->id;
-        });
-
-        $this->dontSeeIsAuthenticated();
-
-        $this->see('Enviamos a tu email un enlace para que inicies sesion');
-    }
-
-    function test_a_guest_user_can_request_a_token_whitout_an_email()
-    {
-        // Having
-        Mail::fake();
+        $token = Token::generateFor($user);
 
         // When
-        $this->visitRoute('login')
-            ->press('Solicitar token');
+        $this->visit("login/{$token->token}");
 
-        // Then: a new token is NOT created in the database
-        $token = Token::first();
+        // Then
+        $this->seeIsAuthenticated()
+            ->seeIsAuthenticatedAs($user);
 
-        $this->assertNull($token, 'A token was created');
-
-        // And sent to the user
-        Mail::assertNotSent(TokenMail::class);
-
-        $this->dontSeeIsAuthenticated();
-
-        $this->seeErrors([
-            'email' => 'El campo correo electrónico es obligatorio'
+        $this->dontSeeInDatabase('tokens', [
+            'id' => $token->id
         ]);
-    }
 
-    function test_a_guest_user_can_request_a_token_an_invalid_email()
-    {
-        // Having
-
-        // When
-        $this->visitRoute('login')
-            ->type('Boster', 'email')
-            ->press('Solicitar token');
-
-        $this->seeErrors([
-            'email' => 'correo electrónico no es un correo válido'
-        ]);
-    }
-
-    function test_a_guest_user_can_request_a_token_with_a_non_existent_email()
-    {
-        $this->defaultUser(['email' => 'hola@jhersy.com']);
-
-        // When
-        $this->visitRoute('login')
-            ->type('boster@jhersy.com', 'email')
-            ->press('Solicitar token');
-
-        $this->seeErrors([
-            'email' => 'Este correo electrónico no existe.'
-        ]);
+        $this->seePageIs('/');
     }
 }
---
+++ tests/features/RequestTokenTest.php
@@ -0,0 +1,87 @@
+<?php
+
+use App\Token;
+use App\Mail\TokenMail;
+use Illuminate\Support\Facades\Mail;
+
+class RequestTokenTest extends FeatureTestCase
+{
+    function test_a_guest_user_can_request_a_token()
+    {
+        // Having
+        Mail::fake();
+
+        $user = $this->defaultUser(['email' => 'hola@jhersy.com']);
+
+        // When
+        $this->visitRoute('token')
+            ->type('hola@jhersy.com', 'email')
+            ->press('Solicitar token');
+
+        // Then: a new token is created in the database
+        $token = Token::where('user_id', $user->id)->first();
+
+        $this->assertNotNull($token, 'A token was not created');
+
+        // And sent to the user
+        Mail::assertSentTo($user, TokenMail::class, function ($mail) use ($token) {
+            return $mail->token->id === $token->id;
+        });
+
+        $this->dontSeeIsAuthenticated();
+
+        $this->see('Enviamos a tu email un enlace para que inicies sesion');
+    }
+
+    function test_a_guest_user_can_request_a_token_whitout_an_email()
+    {
+        // Having
+        Mail::fake();
+
+        // When
+        $this->visitRoute('token')
+            ->press('Solicitar token');
+
+        // Then: a new token is NOT created in the database
+        $token = Token::first();
+
+        $this->assertNull($token, 'A token was created');
+
+        // And sent to the user
+        Mail::assertNotSent(TokenMail::class);
+
+        $this->dontSeeIsAuthenticated();
+
+        $this->seeErrors([
+            'email' => 'El campo correo electrónico es obligatorio'
+        ]);
+    }
+
+    function test_a_guest_user_can_request_a_token_an_invalid_email()
+    {
+        // Having
+
+        // When
+        $this->visitRoute('token')
+            ->type('Boster', 'email')
+            ->press('Solicitar token');
+
+        $this->seeErrors([
+            'email' => 'correo electrónico no es un correo válido'
+        ]);
+    }
+
+    function test_a_guest_user_can_request_a_token_with_a_non_existent_email()
+    {
+        $this->defaultUser(['email' => 'hola@jhersy.com']);
+
+        // When
+        $this->visitRoute('token')
+            ->type('boster@jhersy.com', 'email')
+            ->press('Solicitar token');
+
+        $this->seeErrors([
+            'email' => 'Este correo electrónico no existe.'
+        ]);
+    }
+}

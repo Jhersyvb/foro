9514053d0fc4a0a39126264390c5deef0ae133c0
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-03-07 18:22:00 -0500

    Estilos para la vista de detalle de un post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,47 +3,59 @@
 @section('content')
     <h1>{{ $post->title }}</h1>
 
-    {!! $post->safe_html_content !!}
+    <div class="row">
+        <div class="col-md-10">
+            <p>
+                Publicado por <a href="#">{{ $post->user->name }}</a>
+                {{ $post->created_at->diffForHumans() }}
+                en <a href="{{ $post->category->url }}">{{ $post->category->name }}</a>
+                @if ($post->pending)
+                    <span class="label label-warning">Pendiente</span>
+                @else
+                    <span class="label label-success">Completado</span>
+                @endif
+            </p>
 
-    <p>{{ $post->user->name }}</p>
+            {!! $post->safe_html_content !!}
 
-    @if (auth()->check())
-        @if (!auth()->user()->isSubscribedTo($post))
-            {!! Form::open(['route' => ['posts.subscribe', $post], 'method' => 'POST']) !!}
-                <button type="submit">Suscribirse al post</button>
-            {!! Form::close() !!}
-        @else
-            {!! Form::open(['route' => ['posts.unsubscribe', $post], 'method' => 'DELETE']) !!}
-                <button type="submit">Desuscribirse del post</button>
-            {!! Form::close() !!}
-        @endif
-    @endif
-
-    <h4>Comentarios</h4>
-
-    {!! Form::open(['route' => ['comments.store', $post], 'method' => 'POST']) !!}
-
-        {!! Field::textarea('comment') !!}
-
-        <button type="submit">Publicar comentario</button>
-
-    {!! Form::close() !!}
-
-    @foreach($post->latestComments as $comment)
-        <article class="{{ $comment->answer ? 'answer' : '' }}">
-            {{ $comment->comment }}
-
-            {{--@can('accept', $comment)
-                {!! Form::open(['route' => ['comments.accept', $comment], 'method' => 'POST']) !!}
-                    <button type="submit">Aceptar respuesta</button>
-                {!! Form::close() !!}
-            @endcan--}}
-
-            @if(Gate::allows('accept', $comment) && !$comment->answer)
-                {!! Form::open(['route' => ['comments.accept', $comment], 'method' => 'POST']) !!}
-                <button type="submit">Aceptar respuesta</button>
-                {!! Form::close() !!}
+            @if (auth()->check())
+                @if (!auth()->user()->isSubscribedTo($post))
+                    {!! Form::open(['route' => ['posts.subscribe', $post], 'method' => 'POST']) !!}
+                        <button type="submit">Suscribirse al post</button>
+                    {!! Form::close() !!}
+                @else
+                    {!! Form::open(['route' => ['posts.unsubscribe', $post], 'method' => 'DELETE']) !!}
+                        <button type="submit">Desuscribirse del post</button>
+                    {!! Form::close() !!}
+                @endif
             @endif
-        </article>
-    @endforeach
+
+            <hr>
+
+            <h4>Comentarios</h4>
+
+            @foreach($post->latestComments as $comment)
+                <article class="{{ $comment->answer ? 'answer' : '' }}">
+                    {{ $comment->comment }}
+
+                    @if(Gate::allows('accept', $comment) && !$comment->answer)
+                        {!! Form::open(['route' => ['comments.accept', $comment], 'method' => 'POST']) !!}
+                        <button type="submit">Aceptar respuesta</button>
+                        {!! Form::close() !!}
+                    @endif
+                </article>
+
+                <hr>
+            @endforeach
+
+            {!! Form::open(['route' => ['comments.store', $post], 'method' => 'POST']) !!}
+
+                {!! Field::textarea('comment') !!}
+
+                <button type="submit">Publicar comentario</button>
+
+            {!! Form::close() !!}
+        </div>
+        <div class="col-md-2"></div>
+    </div>
 @endsection
